13f66625b97b2250a9caba0cdf10d5fdd4cd40a5
Larry Shatzer, Jr <larrys@gmail.com>
2012-06-19 14:13:32 -0600

    Add macro token of FAVORITE_USERS_EMAIL, so email-ext can email users who marked the job a favorite.
    Bump up core to 1.447 (LTS version)

---
+++ src/main/java/hudson/plugins/favorite/token/FavoriteUsersEmailTokenMacro.java
@@ -0,0 +1,40 @@
+package hudson.plugins.favorite.token;
+
+import hudson.Extension;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.TaskListener;
+import hudson.model.User;
+import hudson.plugins.favorite.user.FavoriteUserProperty;
+import hudson.tasks.Mailer;
+import org.apache.commons.lang.StringUtils;
+import org.jenkinsci.plugins.tokenmacro.DataBoundTokenMacro;
+import org.jenkinsci.plugins.tokenmacro.MacroEvaluationException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Extension
+public class FavoriteUsersEmailTokenMacro extends DataBoundTokenMacro {
+    @Override
+    public String evaluate(AbstractBuild<?, ?> context, TaskListener listener, String macroName)
+            throws MacroEvaluationException, IOException, InterruptedException {
+        List<String> users = new ArrayList<String>();
+        AbstractProject project = context.getProject();
+        for (User user : User.getAll()) {
+            FavoriteUserProperty fup = user.getProperty(FavoriteUserProperty.class);
+            if (fup != null && fup.isJobFavorite(project.getName())) {
+                // probably various ways to get this across various Jenkins installs.
+                Mailer.UserProperty mail = user.getProperty(Mailer.UserProperty.class);
+                users.add(mail.getAddress());
+            }
+        }
+        return StringUtils.join(users, ",");
+    }
+
+    @Override
+    public boolean acceptsMacroName(String macroName) {
+        return macroName.equals("FAVORITE_USERS_EMAIL");
+    }
+}
