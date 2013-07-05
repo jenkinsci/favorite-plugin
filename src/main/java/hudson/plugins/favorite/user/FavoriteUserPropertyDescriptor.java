079a3c509c5cb27a5135cc78985c0f3065300343
Stephen Connolly <stephen.alan.connolly@gmail.com>
2013-07-05 15:21:11 +0100

    Fix links when in folders

--- src/main/java/hudson/plugins/favorite/user/FavoriteUserPropertyDescriptor.java
+++ src/main/java/hudson/plugins/favorite/user/FavoriteUserPropertyDescriptor.java
@@ -2,9 +2,13 @@ package hudson.plugins.favorite.user;
 
 import hudson.model.AutoCompletionCandidates;
 import hudson.model.Hudson;
+import hudson.model.Item;
+import hudson.model.ItemGroup;
 import hudson.model.User;
 import hudson.model.UserProperty;
 import hudson.model.UserPropertyDescriptor;
+import jenkins.model.Jenkins;
+import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.QueryParameter;
 import hudson.plugins.favorite.Messages;
 
@@ -32,6 +36,24 @@ public class FavoriteUserPropertyDescriptor extends UserPropertyDescriptor {
             }
         }
         return c;
+    }
+
+    public Item toItem(String fullName) {
+        if (StringUtils.isEmpty(fullName)) return null;
+        ItemGroup<? extends Item> container = Jenkins.getInstance();
+        int start = 0;
+        int index = fullName.indexOf('/', start);
+        while (index != -1) {
+            Item item = container.getItem(fullName.substring(start, index));
+            if (item instanceof ItemGroup) {
+                container = (ItemGroup<? extends Item>) item;
+                start = index+1;
+                index = fullName.indexOf('/', start);
+            } else {
+                index = fullName.indexOf('/', index + 1);
+            }
+        }
+        return container.getItem(fullName.substring(start));
     }
 
 }
