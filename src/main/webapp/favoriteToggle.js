70e015abe18980e3b460092507036949585e5d43
Bernhard Grünewaldt <github@gruenewaldt.net>
2016-11-20 00:52:31 +0100

    check for attribute before setting src. Allows svg icon replacement

--- src/main/webapp/favoriteToggle.js
+++ src/main/webapp/favoriteToggle.js
@@ -2,12 +2,16 @@ function toggleFavorite(job, user, a) {
   new Ajax.Request(rootURL+'/plugin/favorite/toggleFavorite?job=' + job + "&userName=" + user);
   image = document.getElementById("fav_" + job);
   if (image.classList.contains("icon-fav-inactive")) {
-    image.src = image.src.replace("star.png", "star-gold.png");
+    if (image.hasAttribute("src")) {
+      image.src = image.src.replace("star.png", "star-gold.png");
+    }
     image.classList.add("icon-fav-active");
     image.classList.remove("icon-fav-inactive");
     hoverNotification('Favorite added', a.parentNode);
   } else {
-    image.src = image.src.replace("star-gold.png", "star.png");
+    if (image.hasAttribute("src")) {
+      image.src = image.src.replace("star-gold.png", "star.png");
+    }
     image.classList.add("icon-fav-inactive");
     image.classList.remove("icon-fav-active");
     hoverNotification('Favorite deleted', a.parentNode);
