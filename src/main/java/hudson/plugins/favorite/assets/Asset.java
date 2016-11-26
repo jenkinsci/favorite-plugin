c31fc0656506e250e10e098dc1f557cfd51e76f8
Bernhard Grünewaldt <github@gruenewaldt.net>
2016-11-26 14:26:58 +0100

    [JENKINS-39990] correct JavaDoc error

--- src/main/java/hudson/plugins/favorite/assets/Asset.java
+++ src/main/java/hudson/plugins/favorite/assets/Asset.java
@@ -2,7 +2,7 @@ package hudson.plugins.favorite.assets;
 
 /**
  * Just a dummy class for the jelly st:include which otherwise throws exceptions
- * in config.jelly when <st:include></st:include> to css.jelly is called without class parameter
+ * in config.jelly when st:include to css.jelly is called without class parameter
  */
 public class Asset {
 }
