87592a23c21a49143aff9d85c41ff4b15e753b94
Jesse Glick <jglick@cloudbees.com>
2017-10-19 17:15:29 -0400

    [FIXED JENKINS-44797] Switch to adjuncts.

--- src/main/java/hudson/plugins/favorite/assets/Asset.java
+++
@@ -1,8 +0,0 @@
-package hudson.plugins.favorite.assets;
-
-/**
- * Just a dummy class for the jelly st:include which otherwise throws exceptions
- * in config.jelly when st:include to css.jelly is called without class parameter
- */
-public class Asset {
-}
