89529ea8153920cc9cdb9c3d7fa0636128812068
Markus Winter <m.winter@sap.com>
2023-12-10 02:13:19 +0100

    unify jelly files
    remove not required things

--- src/main/resources/hudson/plugins/favorite/assets.js
+++ src/main/resources/hudson/plugins/favorite/assets.js
@@ -1,38 +1,30 @@
-function toggleFavoriteOnClick(event, a) {
+Behaviour.specify("A.favorite-toggle", "favorite.toggle", 0, (a) => {
+  a.onclick = (event) => {
     event.preventDefault();
-    return toggleFavorite(a.getAttribute("data-fullName"), a);
-}
-
-function toggleFavorite(job, a) {
-  fetch(rootURL + "/plugin/favorite/toggleFavorite?job=" + encodeURIComponent(job), {
-    method: 'post',
-    headers: crumb.wrap({}),
-  });
-  var favIcon = document.getElementById("fav_" + job);
-  if (favIcon.getAttribute("data-fav") === 'false') {
-    favIcon.setAttribute('data-fav', 'true');
-    favIcon.querySelector('.icon-fav-inactive').classList.add('jenkins-hidden');
-    favIcon.querySelector('.icon-fav-active').classList.remove('jenkins-hidden');
-    hoverNotification("Favorite added", a.parentNode);
-    window.dispatchEvent(new CustomEvent('favorite-plugin-icon-change', {
-      transition: "active-to-inactive",
-      job: job
-    }));
-  } else {
-    favIcon.setAttribute('data-fav', 'false');
-    favIcon.querySelector('.icon-fav-inactive').classList.remove('jenkins-hidden');
-    favIcon.querySelector('.icon-fav-active').classList.add('jenkins-hidden');
-    hoverNotification("Favorite deleted", a.parentNode);
-    window.dispatchEvent(new CustomEvent('favorite-plugin-icon-change', {
-      transition: "inactive-to-active",
-      job: job
-    }));
-  }
-  return false;
-}
-
-Behaviour.specify("A.favorite-toggle", "favorite.toggle", 0, (element) => {
-  element.onclick = (event) => {
-    toggleFavoriteOnClick(event, element);
+    const job = a.getAttribute("data-fullName");
+    fetch(rootURL + "/plugin/favorite/toggleFavorite?job=" + encodeURIComponent(job), {
+      method: 'post',
+      headers: crumb.wrap({}),
+    });
+    if (a.getAttribute("data-fav") === 'false') {
+      a.setAttribute('data-fav', 'true');
+      a.querySelector('.icon-fav-inactive').classList.add('jenkins-hidden');
+      a.querySelector('.icon-fav-active').classList.remove('jenkins-hidden');
+      hoverNotification("Favorite added", a.parentNode);
+      window.dispatchEvent(new CustomEvent('favorite-plugin-icon-change', {
+        transition: "active-to-inactive",
+        job: job
+      }));
+    } else {
+      a.setAttribute('data-fav', 'false');
+      a.querySelector('.icon-fav-inactive').classList.remove('jenkins-hidden');
+      a.querySelector('.icon-fav-active').classList.add('jenkins-hidden');
+      hoverNotification("Favorite deleted", a.parentNode);
+      window.dispatchEvent(new CustomEvent('favorite-plugin-icon-change', {
+        transition: "inactive-to-active",
+        job: job
+      }));
+    }
+    return false;
   }
 });
